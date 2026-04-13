4e4a7ff84c4dab5e027256b3630fd9f58649c24e
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-04-13 13:56:04 -0600

    Cajero

---
+++ Cajero/Program.cs
@@ -0,0 +1,21 @@
+﻿var cuenta = new CuentaBancaria("Juan Pérez");
+
+// cuenta.Saldo = 5000;
+
+// cuenta.Saldo = -999999;
+
+try
+{
+	cuenta.Depositar(1500.50m);
+	cuenta.Retirar(200.00m);
+
+	cuenta.Retirar(-500);
+}
+catch (ArgumentException ex)
+{
+	Console.ForegroundColor = ConsoleColor.Red;
+	Console.WriteLine($"ALERTA DE SEGURIDAD: {ex.Message}");
+	Console.ResetColor();
+}
+
+Console.WriteLine($"Saldo final: ${cuenta.Saldo:N2}");
