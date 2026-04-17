3be59759330d4e66da60232e12ff94e72c54fb8f
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-04-17 12:50:57 -0600

    Encapsulación

---
+++ DronApp/Program.cs
@@ -0,0 +1,17 @@
+﻿var motor = new ConfiguracionMotor(500, 25);
+
+// motor.TemperaturaCelsius = 85;
+
+motor.ActualizarLecturaSensor(85);
+motor.AplicarAceleracion();
+
+try
+{
+	motor.ActualizarLecturaSensor(-200);
+}
+catch (ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine($"Protección activa: {ex.Message}");
+}
+
+Console.WriteLine($"Estado final: {motor.TemperaturaCelsius:F1}°C");
---
+++ EdificioApp/Termostato.cs
@@ -0,0 +1,75 @@
+﻿// Código bajo análisis crítico (Caso #3)
+public class Termostato
+{
+    private double _temperaturaObjetivo;
+    private readonly double _temperaturaMinima;
+    private readonly double _temperaturaMaxima;
+    private readonly double _incrementoEstandar;
+
+    // Control de tasa sin cambio
+    private DateTime _ultimoCambio;
+    private int _contadorCambios;
+    private readonly TimeSpan _ventanaTiempo = TimeSpan.FromMinutes(1);
+    private readonly int _maxCambiosPorVentana = 5;
+
+    public Termostato(double tempInicial = 21.0, double incremento = 0.5)
+    {
+        _temperaturaMinima = 10.0;
+        _temperaturaMaxima = 35.0;
+        _incrementoEstandar = incremento;
+
+        _temperaturaObjetivo = ValidarRango(tempInicial);
+        _ultimoCambio = DateTime.Now;
+        _contadorCambios = 0;
+    }
+
+    private double ValidarRango(double valor)
+    {
+        if(valor < _temperaturaMinima)
+        {
+            Console.WriteLine($"[AVISO] {valor:F1}°C es demasiado bajo." +
+                $"Ajustando a mínimo {_temperaturaMinima}°C");
+            return _temperaturaMinima;
+        }
+        if (valor > _temperaturaMaxima)
+        {
+            Console.WriteLine($"[AVISO] {valor:F1}°C es demasiado alto." +
+                $"Ajustando a máximo {_temperaturaMaxima}°C");
+            return _temperaturaMaxima;
+        }
+        return valor;
+    }
+
+    public double TemperaturaObjetivo
+    {
+        get => _temperaturaObjetivo;
+    }
+
+    public double TemperaturaMinima { get  => _temperaturaMinima; }
+    public double TemperaturaMaxima { get  => _temperaturaMaxima; }
+
+    public void SubirTemperatura()
+    {
+        ModificarTemperatura(_incrementoEstandar);
+    }
+
+    private void ModificarTemperatura(double delta)
+    {
+        if(!PuedeModificar())
+        {
+
+        }
+    }
+
+    private bool PuedeModificar()
+    {
+        DateTime ahora = DateTime.Now;
+
+        if(ahora - _ultimoCambio > _ventanaTiempo)
+        {
+            _contadorCambios = 0;
+        }
+
+        return _contadorCambios < _maxCambiosPorVentana;
+    }
+}
