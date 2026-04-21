6719c5eedbd9a0de6807d722bfe38db91b7719f6
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-04-21 11:24:14 -0600

    CuentaPremium

--- Banco/CuentaPremium.cs
+++ Banco/CuentaPremium.cs
@@ -1,7 +1,23 @@
 ﻿public class CuentaPremium : CuentaBancariaBase
 {
+    public CuentaPremium(string numeroCuenta) : 
+        base(numeroCuenta, 100)
+    {
+    }
+
     public void AplicarBeneficioExclusivo()
     {
-        _saldo += 50;
+        Console.WriteLine("Aplicando beneficio " +
+            "de cuenta Premium...");
+        AcreditarInterno(50m, "Bono Bienvenida Premium");
+    }
+
+    protected override void AcreditarInterno(decimal monto, 
+        string concepto)
+    {
+        Console.WriteLine("[Override Premium] " +
+            "Aplicando multiplicador 1.1x por fidelidad.");
+        base.AcreditarInterno(monto * 1.1m, 
+            concepto + " (Multiplicando)");
     }
 }
