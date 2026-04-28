a53ba1b52024ed8ed8f8b5a69a59d5f846e693bb
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-04-28 11:49:15 -0600

    SistemaNomina

---
+++ SistemaNomina/EmpleadoPorComision.cs
@@ -0,0 +1,49 @@
+﻿public class EmpleadoPorComision : Empleado
+{
+    private decimal _tarifaComision;
+    private decimal _ventasBrutas;
+
+    public decimal TarifaComision
+    {
+        get => _tarifaComision;
+        private set
+        {
+            if (value < 0 || value > 1)
+                throw new ArgumentOutOfRangeException
+                    ("La tarifa de comisión debe estar entre 0 y 1");
+            _tarifaComision = value;
+        }
+    }
+
+    public decimal VentasBrutas
+    {
+        get => _ventasBrutas;
+        private set
+        {
+            if (value < 0)
+                throw new ArgumentException
+                   ("Las ventas brutas no pueden ser negativas");
+            _ventasBrutas = value;
+        }
+    }
+    public EmpleadoPorComision(string nombre, string apellido, 
+        decimal tarifaComision, decimal ventasBrutas) 
+        : base(nombre, apellido)
+    {
+        TarifaComision = tarifaComision;
+        VentasBrutas = ventasBrutas;
+    }
+
+    public override decimal CalcularIngresos()
+    {
+        return TarifaComision * VentasBrutas;
+    }
+
+    public override string ObtenerInformacion()
+    {
+        return $"Empleado por Comisión: {NombreCompleto} | " +
+            $"{TarifaComision:P0} comisión | ${VentasBrutas:N0} " +
+            $"en ventas";
+    }
+}
+
---
+++ SistemaNomina/EmpleadoPorHoras.cs
@@ -0,0 +1,61 @@
+﻿public class EmpleadoPorHoras : Empleado
+{
+    private decimal _sueldoPorHora;
+    private int _horasTrabajadas;
+
+    public decimal SueldoPorHora
+    {
+        get => _sueldoPorHora;
+        private set
+        {
+            if(value <= 0)
+                throw new ArgumentException(
+                  "El sueldo por hora debe ser mayor a 0");
+            _sueldoPorHora = value;
+        }
+    }
+
+    public int HorasTrabajadas
+    {
+        get => _horasTrabajadas;
+        private set
+        {
+            if (value <= 0) throw new ArgumentException
+             ("Las horas trabajadas no pueden ser negativas");
+            if (value > 730)
+                throw new ArgumentException
+                 ("Horas exceden el límite mensual");
+            _horasTrabajadas = value;
+        }
+    }
+
+    public EmpleadoPorHoras(string nombre, 
+        string apellido,
+        decimal sueldoPorHora,
+        int horaTrabajadas) : base(nombre, apellido)
+    {
+        SueldoPorHora = sueldoPorHora;
+        HorasTrabajadas = horaTrabajadas;
+    }
+
+    public override decimal CalcularIngresos()
+    {
+        const decimal HORAS_NORMALES = 40;
+        const decimal FACTOR_HORA_EXTRA = 2.0m;
+
+        if(HorasTrabajadas <= HORAS_NORMALES)
+            return SueldoPorHora * HorasTrabajadas;
+        else
+        {
+            decimal horasExtras = HorasTrabajadas - HORAS_NORMALES;
+            return (SueldoPorHora * HORAS_NORMALES) +
+                (SueldoPorHora * FACTOR_HORA_EXTRA * horasExtras);
+        }
+    }
+
+    public override string ObtenerInformacion()
+    {
+        return $"Empleado por Horas: {NombreCompleto} | " +
+            $"{SueldoPorHora}/hora | {HorasTrabajadas} horas";
+    }
+}
