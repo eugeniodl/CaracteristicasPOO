93956491add76f12e36b7600ba72b65cc427b9cb
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-04-30 08:20:22 -0600

    SistemaNomina

--- SistemaNomina/Program.cs
+++ SistemaNomina/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Empleado[] empleados =
+{
+    new EmpleadoPorHoras("Ana", "López", 15.5m, 45),
+    new EmpleadoPorComision("Luis", "Ríos", 0.010m, 5000m),
+    new EmpleadoBaseMasComision("Marta", "Solís", 0.08m, 8000m, 1200m)
+};
+
+foreach (Empleado e in empleados)
+{
+    MostrarIngresos(e);
+    MostrarInformacion(e);
+    DescubrirTipo(e);
+}
+
+void DescubrirTipo(Empleado e)
+{
+    Console.WriteLine($"Tipo real (GetType): {e.GetType().Name}");
+    if (e is EmpleadoBaseMasComision)
+        Console.WriteLine("Este empleado tiene salario base + comision");
+    else if (e is EmpleadoPorComision)
+        Console.WriteLine("Este empleado trabaja solo por comisión");
+    else if (e is EmpleadoPorHoras)
+        Console.WriteLine("Este empleado trabaja por horas");
+}
+
+void MostrarInformacion(Empleado e)
+{
+    Console.WriteLine(e.ObtenerInformacion());
+}
+
+void MostrarIngresos(Empleado e)
+{
+    Console.WriteLine($"{e.NombreCompleto} " +
+        $"gana: {e.CalcularIngresos():F2}");
+}
