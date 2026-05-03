0c893c48faf44960ed20dc2615fa094065da1951
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-05-02 20:22:00 -0600

    SistemaNomina

---
+++ SistemaNomina/Empleado.cs
@@ -0,0 +1,47 @@
+﻿public abstract class Empleado
+{
+    private string? _nombre;
+    private string? _apellido;
+
+    public string? Nombre
+    {
+        get => _nombre;
+        private set
+        {
+            if(string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("El nombre no puede estar vacío");
+            _nombre = value;
+        }
+    }
+
+    public string? Apellido
+    {
+        get => _apellido;
+        private set
+        {
+            if(string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("El apellido no puede estar vacío");
+            _apellido = value;
+        }
+    }
+
+    public string NombreCompleto => $"{Nombre} {Apellido}";
+
+    protected Empleado(string nombre, string apellido)
+    {
+        Nombre = nombre;
+        Apellido = apellido;
+    }
+    /// <summary>
+    /// Método abstracto que cada subclase debe implementar obligatoriamente
+    /// </summary>
+    public abstract decimal CalcularIngresos();
+    /// <summary>
+    /// Método virtual que puede ser sobrescrito opcionalmente
+    /// </summary>
+    /// <returns></returns>
+    public virtual string MostrarInformacion()
+    {
+        return $"Empleado: {NombreCompleto}";
+    }
+}
---
+++ SistemaNomina/EmpleadoPorComision.cs
@@ -0,0 +1,28 @@
+﻿public class EmpleadoPorComision : Empleado
+{
+    private decimal _tarifaComision;
+    private decimal _ventasBrutas;
+
+    public decimal TarifaComision
+    {
+        get => _tarifaComision;
+        private set
+        {
+            if (value < 0 || value > 1)
+                throw new ArgumentOutOfRangeException
+            ("La tarifa de comisión debe estar entre 0 y 1");
+            _tarifaComision = value;
+        }
+    }
+
+    public EmpleadoPorComision(string nombre, 
+        string apellido, decimal tarifaComision,
+        decimal ventasBrutas) : base(nombre, apellido)
+    {
+    }
+
+    public override decimal CalcularIngresos()
+    {
+        throw new NotImplementedException();
+    }
+}
