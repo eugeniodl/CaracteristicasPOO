153ce4ef6384447a278a2504da8978aa2c0ffd36
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-05-15 13:19:03 -0600

    SistemaProcesamientoPagos

--- SistemaProcesamientoPagos/Program.cs
+++ SistemaProcesamientoPagos/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var procesadorPagos = new ProcesadorPagos();
+
+MetodoPago tarjeta =
+    new PagoTarjetaCredito("USD", 100.00m, "1234-5678-9012-3456");
+MetodoPago transferencia =
+    new PagoTransferenciaBancaria("EUR", 200.00m);
+MetodoPago cripto =
+    new PagoCriptoMoneda("BTC", 0.5m, 
+    "0xAbC1234Ef5678Gh9iJkLmNopQrStUvWxYz");
+
+procesadorPagos.Procesar(tarjeta);
+procesadorPagos.Procesar(transferencia);
+procesadorPagos.Procesar(cripto);
