f6a31a104ca74b0dc85efc8632b84f9eff2980c4
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2026-05-16 15:57:16 -0600

    SistemaProcesamientoPagos

---
+++ SistemaProcesamientoPagos/PagoCriptomenda.cs
@@ -0,0 +1,13 @@
+﻿public sealed class PagoCriptomenda : MetodoPago
+{
+    public PagoCriptomenda(string moneda, 
+        decimal montoBase) : base(moneda, montoBase)
+    {
+    }
+
+    public override decimal CalcularComision()
+    {
+        throw new NotImplementedException();
+    }
+}
+
